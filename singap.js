94900628e6868f94975cbbf35c0f01a3ace5d2c3
singap88a <130315264+singap88a@users.noreply.github.com>
2023-11-09 13:28:36 -0800

    Add files via upload

---
+++ singap.js
@@ -0,0 +1,89 @@
+
+ 
+///////////////////////////////////////////////
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+// rotate text js code 
+let text = document.querySelector(".text p");
+
+text.innerHTML = text.innerHTML.split("").map((char,i)=>
+    `<b style="transform:rotate(${i * 10.3}deg")>${char}</b>`
+).join("");
+
+
+// switch between about buttons 
+ 
+
+
+
+// portfolio fillter 
+
+var mixer = mixitup('.portfolio-gallery',{
+    selectors: {
+        target: '.portfolio-box'
+    },
+    animation: {
+        duration: 500
+    }
+});
+
+
+// Initialize swiperjs 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+ 
+
+
+
+ 
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+ 
